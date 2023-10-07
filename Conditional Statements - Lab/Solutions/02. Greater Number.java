8612d0f81dd6f6099571ecddf6683dc279c4dd90
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-07 20:56:20 +0300

    Create 02. Greater Number.java

---
+++ Conditional Statements - Lab/Solutions/02. Greater Number.java
@@ -0,0 +1,18 @@
+package conditionalchecks;
+ 
+import java.util.Scanner;
+ 
+public class P02GreaterNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+ 
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
