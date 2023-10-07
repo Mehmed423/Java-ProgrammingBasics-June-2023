b92b1852a4e921fd004693252daf25937467af27
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-07 20:56:51 +0300

    Create 03. Even or Odd.java

---
+++ Conditional Statements - Lab/Solutions/03. Even or Odd.java
@@ -0,0 +1,18 @@
+package conditionalchecks;
+ 
+import java.util.Scanner;
+ 
+public class P03EvenorOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int number = Integer.parseInt(scanner.nextLine());
+ 
+        if(number % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+ 
+    }
+}
