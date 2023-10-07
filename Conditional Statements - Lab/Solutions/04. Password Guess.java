f0b621ff29df0c1d2c39f4123a5e69feb841279b
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-07 20:57:27 +0300

    Create 04. Password Guess.java

---
+++ Conditional Statements - Lab/Solutions/04. Password Guess.java
@@ -0,0 +1,17 @@
+package conditionalchecks;
+ 
+import java.util.Scanner;
+ 
+public class P04PasswordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        String inputText = scanner.nextLine();
+ 
+        if(inputText.equals("s3cr3t!P@ssw0rd")) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
