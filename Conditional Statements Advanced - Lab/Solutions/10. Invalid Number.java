b8c9ce3582b4e6346c0bc287313e1ded5421b692
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-11 21:28:15 +0300

    Create 10. Invalid Number.java

---
+++ Conditional Statements Advanced - Lab/Solutions/10. Invalid Number.java
@@ -0,0 +1,18 @@
+package advancedConditionalStatements;
+ 
+import java.util.Scanner;
+ 
+public class P10InvalidNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int number = Integer.parseInt(scanner.nextLine());
+ 
+        boolean isValid = number >= 100 && number <= 200 || number == 0;
+ 
+        if(!isValid) {
+            System.out.println("invalid");
+        }
+ 
+    }
+}
