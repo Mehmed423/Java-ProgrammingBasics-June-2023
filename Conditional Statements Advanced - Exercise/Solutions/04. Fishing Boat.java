bef6af0c911f0e883efadac57e54775b235ec28a
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-11 21:33:18 +0300

    Create 04. Fishing Boat.java

---
+++ Conditional Statements Advanced - Exercise/Solutions/04. Fishing Boat.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+ 
+public class FishingBoat {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        // 1. Input
+        int budget = Integer.parseInt(scanner.nextLine());
+        String season = scanner.nextLine();
+        int fishermen = Integer.parseInt(scanner.nextLine());
+ 
+        double shipPrice = 0.00;
+ 
+        // 2. Calculating the ship rent
+        switch (season) {
+            case "Spring":
+                shipPrice = 3000;
+                break;
+            case "Summer":
+            case "Autumn":
+                shipPrice = 4200;
+                break;
+            case "Winter":
+                shipPrice = 2600;
+                break;
+        }
+ 
+        if (fishermen <= 6) {
+            shipPrice = shipPrice - (shipPrice * 0.10);
+        } else if (fishermen <= 11) {
+            shipPrice = shipPrice - (shipPrice * 0.15);
+        } else {
+            shipPrice = shipPrice - (shipPrice * 0.25);
+        }
+ 
+        if (fishermen % 2 == 0 && !season.equals("Autumn")) {
+            shipPrice = shipPrice - (shipPrice * 0.05);
+        }
+ 
+        // 3. Print
+        if (budget >= shipPrice) {
+            System.out.printf("Yes! You have %.2f leva left.", budget - shipPrice);
+        } else {
+            System.out.printf("Not enough money! You need %.2f leva.", shipPrice - budget);
+        }
+    }
+}
