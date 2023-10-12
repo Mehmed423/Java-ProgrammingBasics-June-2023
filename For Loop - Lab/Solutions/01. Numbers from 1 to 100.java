b0a16bdfe55051689c4beecf3a61cc22f0e3a919
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-12 20:47:48 +0300

    Create 01. Numbers from 1 to 100.java

---
+++ For Loop - Lab/Solutions/01. Numbers from 1 to 100.java
@@ -0,0 +1,16 @@
+public class Demo {
+    public static void main(String[] args) {
+        //FOR цикъл - повтаряме нещо определен брой пъти
+        // 1 до 100
+ 
+        //начало: 1
+        //край: 100
+        //повтаряме: отпечатвам числото
+        //промяна: +1
+ 
+        for (int number = 1; number <= 100; number++) {
+            //какво искаме да повторим
+            System.out.println(number);
+        }
+    }
+}
