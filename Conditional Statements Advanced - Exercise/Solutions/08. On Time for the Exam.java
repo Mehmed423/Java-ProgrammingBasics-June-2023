e12115c209e79d83bd5c82b2ec1acd19f7d67d36
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-12 20:45:39 +0300

    Create 08. On Time for the Exam.java

---
+++ Conditional Statements Advanced - Exercise/Solutions/08. On Time for the Exam.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+ 
+public class OnTimeForTheExam {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        int examHour = Integer.parseInt(scanner.nextLine());
+        int examMinutes = Integer.parseInt(scanner.nextLine());
+        int arrivalHour = Integer.parseInt(scanner.nextLine());
+        int arrivalMinutes = Integer.parseInt(scanner.nextLine());
+ 
+        int examTime = (examHour * 60) + examMinutes;
+        int arrivalTime = (arrivalHour * 60) + arrivalMinutes;
+ 
+        int diff = Math.abs(examTime - arrivalTime);
+ 
+        //exam 9:30 -> (9 * 60) + 30 = 570
+        //arrival 9:50 -> (9 * 60) + 50 = 590
+        if (examTime < arrivalTime) {
+            System.out.println("Late");
+            if (diff >= 60) {
+                int hours = diff / 60;
+                int min = diff % 60;
+                System.out.printf("%d:%02d hours after the start", hours, min);
+            } else {
+                System.out.printf("%d minutes after the start", diff);
+            }
+        } else if (examTime == arrivalTime || diff <= 30) {
+            System.out.println("On Time");
+            if (diff >= 1) {
+                System.out.printf("%d minutes before the start", diff);
+            }
+        } else {
+            System.out.println("Early");
+            if (diff >= 60) {
+                int hours = diff / 60;
+                int min = diff % 60;
+                System.out.printf("%d:%02d hours before the start", hours, min);
+            } else {
+                System.out.printf("%d minutes before the start", diff);
+            }
+        }
+    }
+}
