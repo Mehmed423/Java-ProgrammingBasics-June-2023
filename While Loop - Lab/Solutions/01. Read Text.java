54d1717590f06a0c1d303f1a43aeeeceac2e807b
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-16 01:23:49 +0300

    Create 01. Read Text.java

---
+++ While Loop - Lab/Solutions/01. Read Text.java
@@ -0,0 +1,16 @@
+package whileLoop;
+ 
+import java.util.Scanner;
+ 
+public class P01ReadText {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        String input = scanner.nextLine();
+ 
+        while(!input.equals("Stop")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+    }
+}
