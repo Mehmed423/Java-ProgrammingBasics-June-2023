1831f908c1ff4fed6388ea03fadb1c975d8a8bc4
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-15 22:59:22 +0300

    Create 10. Odd Even Sum.java

---
+++ For Loop - Lab/Solutions/10. Odd Even Sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class OddEvenSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int n = Integer.parseInt(scanner.nextLine());
+ 
+        int oddSum = 0;
+        int evenSum = 0;
+ 
+        for (int i = 1; i <= n ; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+ 
+            if (i % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+        }
+ 
+        if (oddSum == evenSum) {
+            System.out.println("Yes");
+            System.out.println("Sum = " + oddSum);
+        } else {
+            System.out.println("No");
+            System.out.println("Diff = " + Math.abs(oddSum - evenSum));
+        }
+    }
+}
