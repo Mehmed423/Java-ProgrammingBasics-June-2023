843b1394f0601f8c813cd455749182a07d1e6cb5
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-19 00:46:32 +0300

    Create 08. Graduation.java

---
+++ While Loop - Lab/Solutions/08. Graduation.java
@@ -0,0 +1,37 @@
+package whileLoop;
+ 
+import java.util.Scanner;
+ 
+public class P08Graduation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        String studentName = scanner.nextLine();
+        int classYear = 1;
+        double sumGrade = 0;
+        int timesExpeled = 0;
+ 
+        while (classYear <= 12) {
+            if(timesExpeled > 1) {
+                break;
+            }
+ 
+            double grade = Double.parseDouble(scanner.nextLine());
+            if(grade < 4) {
+                timesExpeled++;
+                continue;
+            }
+ 
+            sumGrade += grade;
+            classYear++;
+        }
+ 
+        double averageGrade = sumGrade / 12;
+        if(timesExpeled > 1) {
+            System.out.printf("%s has been excluded at %d grade", studentName, classYear);
+        } else {
+            System.out.printf("%s graduated. Average grade: %.2f%n", studentName, averageGrade);
+        }
+ 
+    }
+}
