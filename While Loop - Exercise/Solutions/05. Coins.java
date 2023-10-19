5b1fc18d43fe23794f3605e3834e5213cbcc69f5
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-20 00:05:06 +0300

    Create 05. Coins.java

---
+++ While Loop - Exercise/Solutions/05. Coins.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+ 
+public class Coins {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        double change = Double.parseDouble(scanner.nextLine());
+        double sum = Math.round(change * 100);
+ 
+        int coins = 0;
+ 
+        while (sum > 0) {
+            if (sum - 200 >= 0) {
+                sum -= 200;
+                coins++;
+            } else if (sum - 100 >= 0){
+                sum -= 100;
+                coins++;
+            } else if (sum - 50 >= 0){
+                sum -= 50;
+                coins++;
+            } else if (sum - 20 >= 0){
+                sum -= 20;
+                coins++;
+            } else if (sum - 10 >= 0){
+                sum -= 10;
+                coins++;
+            } else if (sum - 5 >= 0){
+                sum -= 5;
+                coins++;
+            } else if (sum - 2 >= 0){
+                sum -= 2;
+                coins++;
+            } else {
+                sum -= 1;
+                coins++;
+            }
+        }
+ 
+        System.out.println(coins);
+ 
+    }
+}
