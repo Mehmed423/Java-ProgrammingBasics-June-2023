e52e5f464476dec30d795f93e5d313e5633a5468
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-21 20:34:16 +0300

    Create 01. Clock.java

---
+++ Nested Loops - Lab/Solutions/01. Clock.java
@@ -0,0 +1,13 @@
+package nestedLoops;
+ 
+public class P01Clock {
+    public static void main(String[] args) {
+ 
+        for(int h = 0; h <= 23; h++) {
+ 
+            for(int m = 0; m <= 59; m++) {
+                System.out.printf("%d:%d%n", h, m);
+            }
+        }
+    }
+}
