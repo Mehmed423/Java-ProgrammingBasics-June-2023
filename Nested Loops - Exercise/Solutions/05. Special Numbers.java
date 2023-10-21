1a6d9473198811a2ae4294500029f518c3150e57
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-22 00:04:38 +0300

    Create 05. Special Numbers.java

---
+++ Nested Loops - Exercise/Solutions/05. Special Numbers.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+ 
+public class SpecialNumbers {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        int myNumber = Integer.parseInt(scanner.nextLine());
+ 
+        for (int currentNumber = 1111; currentNumber <= 9999; currentNumber++) {
+ 
+            boolean isSpecial = true;
+            String textNumber = String.valueOf(currentNumber);
+ 
+            for (int position = 0; position < textNumber.length(); position++) {
+ 
+                int digit = Integer.parseInt(String.valueOf(textNumber.charAt(position)));
+ 
+                if (digit == 0) {
+                    isSpecial = false;
+                    break;
+                }
+ 
+                if (myNumber % digit != 0) {
+                    isSpecial = false;
+                    break;
+                }
+            }
+ 
+            if (isSpecial) {
+                System.out.print(currentNumber + " ");
+            }
+        }
+ 
+    }
+}
