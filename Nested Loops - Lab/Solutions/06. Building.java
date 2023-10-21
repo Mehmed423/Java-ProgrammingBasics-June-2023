ef7977d56c7d68302b4c83e6a0ce079cb18374d3
Mehmed Mehmed <119522797+Mehmed423@users.noreply.github.com>
2023-10-21 20:40:06 +0300

    Create 06. Building.java

---
+++ Nested Loops - Lab/Solutions/06. Building.java
@@ -0,0 +1,27 @@
+package nestedLoops;
+ 
+import java.util.Scanner;
+ 
+public class P06Building {
+    public static void main(String[] args) {
+ 
+        Scanner scanner = new Scanner(System.in);
+ 
+        int floor = Integer.parseInt(scanner.nextLine());
+        int room = Integer.parseInt(scanner.nextLine());
+ 
+        for (int i = floor; i >= 1 ; i--) {
+            for(int k = 0; k < room; k++) {
+                if(i == floor) {
+                    System.out.printf("L%d%d ", i, k);
+                } else if(i % 2 == 0) {
+                    System.out.printf("O%d%d ", i, k);
+                } else {
+                    System.out.printf("A%d%d ", i, k);
+                }
+            }
+            System.out.println();
+ 
+        }
+    }
+}
